0661017914e4801af1258f6e845dd7f3f75871a6
Brian Putnam <brian@brian-putnam.com>
2015-05-22 20:29:35 -0400

    Can format numbers between -1m and 1m

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ fn it_works() {
 		TestExpectation { input: 7, expectation: "seven" },
 		TestExpectation { input: 8, expectation: "eight" },
 		TestExpectation { input: 9, expectation: "nine" },
+
 		TestExpectation { input: 10, expectation: "ten" },
 		TestExpectation { input: 11, expectation: "eleven" },
 		TestExpectation { input: 12, expectation: "twelve" },
@@ -27,6 +28,7 @@ fn it_works() {
 		TestExpectation { input: 17, expectation: "seventeen" },
 		TestExpectation { input: 18, expectation: "eighteen" },
 		TestExpectation { input: 19, expectation: "nineteen" },
+
 		TestExpectation { input: 20, expectation: "twenty" },
 		TestExpectation { input: 21, expectation: "twenty-one" },
 		TestExpectation { input: 22, expectation: "twenty-two" },
@@ -45,10 +47,17 @@ fn it_works() {
 		TestExpectation { input: 79, expectation: "seventy-nine" },
 		TestExpectation { input: 89, expectation: "eighty-nine" },
 		TestExpectation { input: 99, expectation: "ninety-nine" },
+
 		TestExpectation { input: 100, expectation: "one hundred" },
 		TestExpectation { input: 234, expectation: "two hundred thirty-four" },
 		TestExpectation { input: 456, expectation: "four hundred fifty-six" },
-		TestExpectation { input: 999, expectation: "nine hundred ninety-nine" }
+		TestExpectation { input: 999, expectation: "nine hundred ninety-nine" },
+
+		TestExpectation { input: 1000, expectation: "one thousand" },
+		TestExpectation { input: 2345, expectation: "two thousand three hundred forty-five" },
+		TestExpectation { input: 34567, expectation: "thirty-four thousand five hundred sixty-seven" },
+		TestExpectation { input: 456789, expectation: "four hundred fifty-six thousand seven hundred eighty-nine" }
+
 	];
 
 	for test in tests {
@@ -60,8 +69,8 @@ fn format(num: i32) -> String {
 	if num < 0 {
 		"negative ".to_string() + &format(-num)
 	}
-	else if num >= 0 && num < 1000 {
-		format_lt_thousand(num)
+	else if num >= 0 && num < 1000000 {
+		format_lt_million(num)
 	}
 	else {
 		panic!("Unable to format number: {}", num)
@@ -146,6 +155,24 @@ fn format_lt_thousand(num: i32) -> String {
 		}
 		else {
 			hundreds_str + " " + &format_lt_hundred(tens)
+		}
+	}
+}
+
+fn format_lt_million(num: i32) -> String {
+	if num < 1000 {
+		format_lt_thousand(num)
+	}
+	else {
+		let thousands_place = num / 1000;
+		let hundreds = num % 1000;
+		let thousands_str = format_lt_thousand(thousands_place) + " thousand";
+
+		if hundreds == 0 {
+			thousands_str
+		}
+		else {
+			thousands_str + " " + &format_lt_thousand(hundreds)
 		}
 	}
 }
